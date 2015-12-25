7affc811dd6f2a1636a0e2a58283d963b7c4c1c6
Kent Nagata <ngtknt@me.com>
2015-12-25 15:54:55 +0900

    fix shasum

--- Formula/bundlerenv.rb
+++ Formula/bundlerenv.rb
@@ -2,7 +2,7 @@ class Bundlerenv < Formula
   desc "use Bundler version specified by Gemfile.lock"
   homepage "https://github.com/ngtk/bundlerenv"
   url "https://github.com/ngtk/bundlerenv/archive/v0.3.0.tar.gz"
-  sha256 "fd10c96a23448799b79fb85a53f498473a4ad9fa0c4d7d3b7eac259dc549458f"
+  sha256 "4a868dbabf0cb1a60f1e7b81d5e0ef0d8e1f65305063fc80b4e3a530c798cc67"
 
   def install
     system "make", "install", "prefix=#{prefix}"
