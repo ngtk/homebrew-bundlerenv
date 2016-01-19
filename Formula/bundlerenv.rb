f61a227f81c17757edc0ea285b98b36d68a3cc30
Kent Nagata <ngtknt@me.com>
2016-01-19 12:33:08 +0900

    update to v0.3.1

--- Formula/bundlerenv.rb
+++ Formula/bundlerenv.rb
@@ -1,8 +1,8 @@
 class Bundlerenv < Formula
   desc "use Bundler version specified by Gemfile.lock"
   homepage "https://github.com/ngtk/bundlerenv"
-  url "https://github.com/ngtk/bundlerenv/archive/v0.3.0.tar.gz"
-  sha256 "4a868dbabf0cb1a60f1e7b81d5e0ef0d8e1f65305063fc80b4e3a530c798cc67"
+  url "https://github.com/ngtk/bundlerenv/archive/v0.3.1.tar.gz"
+  sha256 "7c9a914244e8bfc96d3605218e4d531c7160b309d93b194585a4b0f668d23a1a"
 
   def install
     system "make", "install", "prefix=#{prefix}"
